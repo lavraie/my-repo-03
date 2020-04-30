cc3c10ed7953e2d643dcc27e264772e1324e329f
lavraie <caesartorrent01@outlook.com>
2020-04-30 15:57:28 +0200

    1

--- z05.js
+++ z05.js
@@ -14,3 +14,38 @@ function dynamicSort(property) {
         }        
     }
 }
+
+async function getData() {
+    fetch('https://datahub.io/core/world-cities/r/world-cities.json')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        var data2 = data.map(({
+          name
+        }) => ({
+          name
+  
+        }));
+        var data3 = data.filter(function(item) {
+          return item.country == "Belgium";
+        });
+        console.log(data3);
+  
+  
+        data3.forEach(function(item) {
+  
+          const newElement = document.createElement("li");
+          const eventList = document.getElementById("ul1");
+  
+          newElement.innerHTML = JSON.stringify(item.name + " " + item.country);
+          eventList.appendChild(newElement);
+  
+  
+        });
+      })
+  };
+  
+  getData();
+  
