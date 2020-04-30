3d481a6db178d6e764381f67cc3b1d43599468b4
lavraie <caesartorrent01@outlook.com>
2020-04-30 09:47:17 +0200

    1

--- z01.js
+++ z01.js
@@ -1,41 +1,46 @@
 async function getData() {
-    fetch("https://restcountries.eu/rest/v2/all")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        var data1 = [];
-        data.forEach(function (item) {
-          data1.push(item);
-        });
-        console.log(data1);
-        console.log(typeof data1);
-  
-        console.log(data1[52]);
-        console.log(data1[51]);
-        //data1.forEach(function(item){
-  
-        //JSON.stringify(data1);
-        //});
-  
-        /* data1.push(data) */
-  
-        /* console.log(data) */
-  
-        /* console.log(data1) */
-  
-        /* document.getElementById("p1").textContent = JSON.stringify(data) */
-        /* document.getElementById("p1").textContent = JSON.stringify((data), ['name', 'capital']) */
-  
-        document.getElementById("p1").textContent = JSON.stringify(data1, [
-          "name",
-          "capital",
-        ]);
-  
-        //});
+  fetch("https://restcountries.eu/rest/v2/all")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      var data1 = [];
+      var data2 = [];
+      //var data1 = data.map((item) => item.name && item.capital));
+
+      data.forEach(function (item) {
+        data1.push(item.name, item.capital);
       });
-  
-  }
-    getData();
-  
-  
+      console.log(data1);
+      var data2 = data.map(({ name, area }) => ({ name, area }));
+      console.log(data2);
+
+      //console.log(data1);
+      console.log("typeof data1: " + typeof data1);
+      console.log("typeof data2: " + typeof data2);
+
+      //console.log(data1[52]);
+      //console.log(data1[51]);
+      //data1.forEach(function(item){
+
+      //JSON.stringify(data1);
+      //});
+
+      /* data1.push(data) */
+
+      /* console.log(data) */
+
+      /* console.log(data1) */
+
+      /* document.getElementById("p1").textContent = JSON.stringify(data) */
+      /* document.getElementById("p1").textContent = JSON.stringify((data), ['name', 'capital']) */
+
+      document.getElementById("p1").textContent = JSON.stringify(data1, [
+        "name",
+        "capital",
+      ]);
+
+      //});
+    });
+}
+getData();
