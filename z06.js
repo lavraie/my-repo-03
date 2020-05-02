2439be40971784918a5f7181d89ce778c5d83eb1
lavraie <caesartorrent01@outlook.com>
2020-05-02 13:28:07 +0200

    1

---
+++ z06.js
@@ -0,0 +1,19 @@
+<p class ="app-title" id="p1">
+
+</p>
+
+var r = "roro";
+
+document.getElementById("p1").innerHTML = `Hello ${r} ${r}
+<p>Hi</p> <p> <br> </p> <p class="footer">Bjr</p>
+`;
+
+
+.app-title{
+    text-align: center
+  }
+  
+  .footer{
+      text-align: left
+  
+  }
