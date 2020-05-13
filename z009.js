aa5eeef6d9f4bf0da637b4f5d8badff9e53db1fb
lavraie <63726257+lavraie@users.noreply.github.com>
2020-05-13 11:52:54 +0200

    1

---
+++ z009.js
@@ -0,0 +1,18 @@
+var n = "nam1";
+
+function printn(name){
+console.log('hey ' + name);
+console.log('hey ' + n);
+
+
+}
+
+printn("h1");
+
+--------------------------------------------
+
+const max = Number.MAX_SAFE_INTEGER;
+console.log(max);
+
+--------------------------------------------
+
